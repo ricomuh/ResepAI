9d03a8909b52c5af8a780fdbc2d65767c40232ff
ricomuh <rico.smartlolo@gmail.com>
2023-03-15 00:31:58 +0700

    removing comments

--- app/generate/page.tsx
+++ app/generate/page.tsx
@@ -1,6 +1,6 @@
 import Form from "@/components/form";
 import Logo from "@/components/logo";
-import ClientOnly from "@/components/ClientOnly";
+import Recipes from "@/components/recipes";
 // import RecipeWrapper from "./recipeWrapper";
 import SubmitForm from "@/components/submit";
 import { Metadata } from "next";
@@ -10,38 +10,6 @@ export interface Result {
   bahan: string[];
   steps: string[];
 }
-
-/*
-async function getRecipes(ingredients: string) {
-  const result = await fetch(
-    process.env.NEXT_PUBLIC_API_URL + "/get?ingredients=" + ingredients,
-    {
-      cache: "no-store",
-    }
-  );
-  return result.json();
-}
-
-export async function generateMetadata({
-  searchParams,
-}: {
-  searchParams: { [key: string]: string | string[] | undefined };
-}) {
-  const recipes: Result = await getRecipes(searchParams.ingredients as string);
-  return {
-    title: `ResepAI - Resep ${recipes.title}`,
-    description: `Resep ${recipes.title} dengan bahan ${recipes.bahan.join(
-      ", "
-    )}`,
-    openGraph: {
-      title: `ResepAI - Resep ${recipes.title}`,
-      description: `Resep ${recipes.title} dengan bahan ${recipes.bahan.join(
-        ", "
-      )}`,
-    },
-  };
-}
-*/
 
 export const metadata: Metadata = {
   title: "ResepAI",
@@ -53,16 +21,12 @@ export const metadata: Metadata = {
 };
 
 export default async function Generate() {
-  // const recipes: Result = await getRecipes(searchParams.ingredients as string);
-
   return (
     <div className="flex flex-col items-center justify-center h-full py-10 gap-3 w-full">
-      {/* <p>{JSON.stringify(searchParams)}</p> */}
       <Logo />
       <Form />
       <SubmitForm />
-      <ClientOnly />
-      {/* <RecipeWrapper /> */}
+      <Recipes />
     </div>
   );
 }
