91f1e393e50f19636344ebb149802975bb6bad35
ricomuh <rico.smartlolo@gmail.com>
2023-03-15 00:32:15 +0700

    setting fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,21 @@
 import "./globals.css";
-import Ingredients from "@/components/ingredientsContext";
 import { Metadata } from "next";
+import { Inter } from "next/font/google";
+
 import Header from "@/components/header";
+import Footer from "@/components/footer";
+import Ingredients from "@/components/ingredientsContext";
 
 export const metadata: Metadata = {
   title: "ResepAI",
   description: "ResepAI adalah aplikasi yang dapat membuat resep makanan",
 };
+
+const inter = Inter({
+  display: "swap",
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
 
 export default function RootLayout({
   children,
@@ -15,12 +24,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="min-h-screen bg-gray-800 flex flex-col justify-between">
+      <body
+        className={`min-h-screen bg-gray-800 flex flex-col justify-between ${inter.className}`}
+      >
         <Header />
         <Ingredients>
           <main className="w-full md:w-8/12 mx-auto my-auto">{children}</main>
         </Ingredients>
-        <div></div>
+        <Footer />
       </body>
     </html>
   );
--- components/logo.tsx
+++ components/logo.tsx
@@ -1,9 +1,18 @@
 import Link from "next/link";
+import { Noto_Sans } from "next/font/google";
+
+const notoSans = Noto_Sans({
+  display: "swap",
+  subsets: ["latin"],
+  weight: "600",
+});
 
 export default function Logo() {
   return (
     <Link href="/">
-      <h1 className="text-6xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-indigo-600 pb-2 drop-shadow-lg duration-200">
+      <h1
+        className={`text-6xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-r from-pink-500 via-y to-indigo-600 pb-2 drop-shadow-lg duration-200 ${notoSans.className}`}
+      >
         ResepAI
       </h1>
     </Link>
